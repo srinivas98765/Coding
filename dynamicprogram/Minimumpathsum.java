b9f99eecb0631113a97acedba410cd5f877622d5
Srinivase1204@ <55191480+srinivas98765@users.noreply.github.com>
2026-05-15 07:34:53 +0530

    Implement dynamic programming for minimum path sum

--- dynamicprogram/Minimumpathsum.java
+++ dynamicprogram/Minimumpathsum.java
@@ -21,3 +21,29 @@ int minPathSum(int[][] grid, int row, int col, int[][] dp){
 Math.min(minPathSum(grid,row-1,col,dp),minPathSum(grid,row,col-1,dp));
 }
 }
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int rows=grid.length;
+        int cols=grid[0].length;
+        int[][] dp=new int[rows][cols];
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<rows;i++)
+        {
+            dp[i][0]=grid[i][0]+dp[i-1][0];
+        }
+        for(int j=1;j<cols;j++)
+        {
+         dp[0][j]=grid[0][j]+dp[0][j-1];       
+        }
+        for(int i=1;i<rows;i++)
+        {
+            for(int j=1;j<cols;j++)
+            {
+                dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    //    System.out.println(dp[0]);
+        return dp[rows-1][cols-1];
+    }
+}
